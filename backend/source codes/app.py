9c464b97256208b4edd752e66889e839fff15232
Liuyuan <2787141886@qq.com>
2023-03-31 09:41:53 +0800

    delete unnecessary code

--- backend/source codes/app.py
+++ backend/source codes/app.py
@@ -5,15 +5,13 @@ app = Flask(__name__, template_folder='../templates')
 
 # 注册名为login_bp的蓝图
 app.register_blueprint(login_bp)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'mydatabase'
+
 
 # 主页面路由
 @app.route('/')
 def index():
     return redirect(url_for('login.login_form'))
+
 
 if __name__ == '__main__':
     app.run()
